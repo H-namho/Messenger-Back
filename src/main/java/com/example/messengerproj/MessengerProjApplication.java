2bab597537de9c36288a2a9390494ed5b8ae73f4
H-namho <skagh9696@gmail.com>
2026-03-12 10:56:05 +0900

    초기 프로젝트 세팅

---
+++ src/main/java/com/example/messengerproj/MessengerProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.messengerproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/messengerproj/MessengerProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messengerproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
